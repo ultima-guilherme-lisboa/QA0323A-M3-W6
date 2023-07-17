39bc108e8cb6a115f82b00435c4d844d72f32a49
gllisboa <guilhermeluzlisboa@gmail.com>
2023-07-18 00:03:54 +0100

    fucionalidade dev

---
+++ lista-produtos.js
@@ -0,0 +1,16 @@
+
+export default class ListaProdutos {
+
+    abrirPaginaHOme () {
+        console.log("Abrir home")
+    }
+
+    static listaProdutos () {
+        console.log("Lista produtos")
+    }
+
+    static getProduto (id) {
+        console.log("Retorna detalhe do produto")
+    }
+}
+
